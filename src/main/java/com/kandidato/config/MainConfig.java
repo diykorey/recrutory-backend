199d532c40dc6ccb2d0cc964a3744e47b5d2629a
Mykola Kavf <kavf.mukola@gmail.com>
2014-03-25 19:46:16 +0200

    Add usage Spring Social (but only partially because of AppEngine )

---
+++ src/main/java/com/kandidato/config/MainConfig.java
@@ -0,0 +1,16 @@
+package com.kandidato.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@ComponentScan(basePackages = "com.kandidato", excludeFilters = {@Filter(Configuration.class)})
+@PropertySource("classpath:com/kandidato/service/linkedin/auth/linkedin-auth.properties")
+public class MainConfig {
+
+}
