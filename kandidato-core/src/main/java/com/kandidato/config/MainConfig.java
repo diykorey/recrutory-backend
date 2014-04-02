1043e30c1f38226888aa176cf7bd94fc98af132d
Andriy Andrunevchyn <diykorey@gmail.com>
2014-04-02 16:42:24 +0300

    created maven multi modules project

--- kandidato-core/src/main/java/com/kandidato/config/MainConfig.java
+++ kandidato-core/src/main/java/com/kandidato/config/MainConfig.java
@@ -4,9 +4,7 @@ package com.kandidato.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 @Configuration
 @ComponentScan(basePackages = "com.kandidato", excludeFilters = {@Filter(Configuration.class)})
