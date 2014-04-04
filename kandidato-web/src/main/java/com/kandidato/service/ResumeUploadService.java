25628e75bc1926e2ba20d85e5085c7da08b092ab
Andriy Andrunevchyn <diykorey@gmail.com>
2014-04-04 18:57:48 +0300

    upload demo

---
+++ kandidato-web/src/main/java/com/kandidato/service/ResumeUploadService.java
@@ -0,0 +1,10 @@
+package com.kandidato.service;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by diyko on 04.04.2014.
+ */
+@Controller
+public class ResumeUploadService {
+}
