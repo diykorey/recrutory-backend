fda2986a6b1d9d6ac7d4fb49ae27afb24c336bd8
Andriy Andrunevchyn <diykorey@gmail.com>
2014-04-13 22:36:45 +0300

    ui

--- kandidato-web/src/main/webapp/resources/custom/js/test.js
+++ kandidato-web/src/main/webapp/resources/custom/js/test.js
@@ -35,8 +35,117 @@ function readActiveVacancies() {
         });
     });
 }
+//add vacncy management
+function Project(projectJson) {
+    this.id = null;
+    this.name = "";
+    this.description = "";
+    if (projectJson) {
+        for (var prop in projectJson) this[prop] = projectJson[prop];
+    }
+}
+function Vacancy(vacancyJson) {
+    this.id = null;
+    this.state = "OPEN";
+    this.hot = false;
+    this.requirements = '';
+    this.tag = [];
+    this.project = new Project()
+    if (vacancyJson) {
+        for (var prop in vacancyJson) this[prop] = vacancyJson[prop];
+    }
+}
+/*   alternative constructor
+ Vacancy.prototype.init = function (vacancyJson) {
+ for (var prop in vacancyJson) this[prop] = vacancyJson[prop];
+ };     */
+
+function getTags(tagsJson) {
+
+    if (!tagsJson) {
+        return "";
+    }
+    var tags = "";
+    $.each(tagsJson, function (key, tagJson) {
+        if (!tags) {
+            tags = tags + ", ";
+        }
+        tags = tags + tagJson["keyword"];
+    });
+    return tags;
+}
+function vacancyView(vacancy) {
+    var title = 'New Vacancy';
+    var saveBtn = 'Save'
+    var vacancyFlowsPanel = '';
+    var tags = getTags(vacancy.tag);
+
+    var vacancyInfoPanel = '<form role="form">' +
+        '<div class="form-group">' +
+        '<label for="vacancyRequirements">Requirements:</label>' +
+        '<textarea id="vacancyRequirements" class="form-control" rows="3">' + vacancy.requirements +
+        '</textarea>' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label for="projectDescription">Project Description:</label>' +
+        '<textarea id="projectDescription" class="form-control" rows="3">' + vacancy.project.description +
+        '</textarea>' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label for="vacancyTags">Tags:</label>' +
+        '<textarea id="vacancyTags" class="form-control" rows="3">' + tags +
+        '</textarea>' +
+        '</div>' +
+        '</form>';
+
+    var vacancyTabPanel = '<div class="tab-content">' +
+        '<div class="tab-pane active" id="vacancyInfoTab">' + vacancyInfoPanel +
+        '</div>' +
+        '<div class="tab-pane" id="vacancyFlowsTab">' + vacancyFlowsPanel +
+        '</div>' +
+        '<div class="tab-pane" id="vacancyCommentsTab">3...</div>' +
+        '<div class="tab-pane" id="vacancyTaskTab">4...</div>' +
+        '</div>';
+    var vacancyNavbar = '<nav class="navbar navbar-default" role="navigation">' +
+        '<div class="container-fluid">' +
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        '<div class="collapse navbar-collapse" id="vacancyNavbar">' +
+        '<ul class="nav navbar-nav" id="vacancyTabs">' +
+        '<li class="active"><a href="#vacancyInfoTab" data-toggle="tab">Main</a></li>' +
+        '<li><a href="#vacancyFlowsTab" data-toggle="tab">Flows</a></li>' +
+        '<li><a href="#vacancyCommentsTab" data-toggle="tab">Comments</a></li>' +
+        '<li><a href="#vacancyTaskTab" data-toggle="tab">Task</a></li>' +
+        '</ul>' +
+        '</div>' +
+        <!-- /.navbar-collapse -->
+        '</div>' +
+        <!-- /.container-fluid -->
+        '</nav>';
+    var vacancyMainPanel = '<div class="panel panel-default">' +
+        '<div class="panel-heading">' + title +
+        '<button type="button" class="btn btn-default btn-xs  pull-right">' +
+        '<span class="glyphicon glyphicon-save"></span>' + saveBtn +
+        '</button>' +
+        '</div>' +
+        ' <div class="panel-body">' + vacancyTabPanel + vacancyNavbar +
+        '</div>' +
+        '</div>';
+    return vacancyMainPanel;
+}
+function createVacancyManager(vacancyJson) {
+    var view;
+    if (vacancyJson) {
+        var vacancy = new Vacancy(vacancyJson);
+        view = vacancyView(vacancy);
+    } else {
+        var vacancy = new Vacancy();
+        view = vacancyView(vacancy);
+    }
+    $("#vacancyManagementHolder").html(view);
+}
 $(document).ready(function () {
     readActiveVacancies();
+    createVacancyManager();
 });
 
 
