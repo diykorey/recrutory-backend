f3eb851c4ec9ea3cb2606a8919265447a81a4f31
apashkev <apashkev@gmail.com>
2014-05-11 19:04:23 +0300

    Allowing access only for USER and ADMIN roles.

--- kandidato-web/src/main/java/com/kandidato/config/SecurityConfig.java
+++ kandidato-web/src/main/java/com/kandidato/config/SecurityConfig.java
@@ -25,6 +25,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().and().authorizeRequests()
-                .anyRequest().authenticated();
+                .anyRequest().hasAnyRole("USER", "ADMIN");
     }
 }
